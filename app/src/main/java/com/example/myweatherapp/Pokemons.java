450c7a1058ff17eb3f1e953b92b4a9422171b63c
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-01 10:05:17 +0200

    ADD POKEDEX

---
+++ app/src/main/java/com/example/myweatherapp/Pokemons.java
@@ -0,0 +1,26 @@
+package com.example.myweatherapp;
+
+//Make class public
+public final class Pokemons {
+
+//Return array method
+    public static String[] getPokemons() {
+        return new String[] {
+                "Pikachu",
+                "Charizard",
+                "Gengar",
+                "Squirtle",
+                "Charmander",
+                "Eeveee",
+                "Treeko",
+                "Grovyle",
+                "Rayquaza",
+                "Sceptile",
+                "Blaziken",
+                "Swampert",
+                "Vaporeon",
+                "Cyndaquill"
+        };
+    }
+
+}
